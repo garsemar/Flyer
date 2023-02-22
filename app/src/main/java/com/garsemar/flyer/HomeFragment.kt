6fda15555ffcbb9138509bbc9ca5f621b7ea3637
garsemar <martigarciaserra@gmail.com>
2023-02-22 19:56:39 +0100

    Navigation component

--- app/src/main/java/com/garsemar/flyer/HomeFragment.kt
+++ app/src/main/java/com/garsemar/flyer/HomeFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.garsemar.flyer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.login.setOnClickListener {
+            val action = FragmentHomeBinding.ac()
+            findNavController().navigate(action)
+        }
     }
 }
--- app/src/main/java/com/garsemar/flyer/MainActivity.kt
+++ app/src/main/java/com/garsemar/flyer/MainActivity.kt
@@ -2,14 +2,16 @@ package com.garsemar.flyer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.garsemar.flyer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Flyer)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // User: itb, pass: superultrasegurapassword
-
-
     }
 }
